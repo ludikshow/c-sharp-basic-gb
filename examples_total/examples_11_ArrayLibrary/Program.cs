26783f2ea61bdaa8ffcf668f041103c8cc7b9ad1
Lyudmila Karpenko <lk@example.com>
2023-02-17 20:48:36 +0300

    lesson 2

---
+++ examples_total/examples_11_ArrayLibrary/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        //index = index + 1;
+        index++;
+    }
+}
+    
+int[] array = new int[10];
+
+FillArray(array);
+
+Console.WriteLine(array[0]);
+
+int Add2Num(int x1, int x2)
+{
+    return x1 + x2;
+}
+
+int num1 = 1;
+int num2 = 8;
+
+Console.WriteLine(Add2Num(num1, num2));
