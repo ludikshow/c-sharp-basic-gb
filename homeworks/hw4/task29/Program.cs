6285fd839ca35c0c63805df883c74a14846c6a1b
Lyudmila Karpenko <lk@example.com>
2023-02-28 19:57:33 +0300

    add task29 solution

---
+++ homeworks/hw4/task29/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.WriteLine("Homework #4");
+
+int[] CreateRandomArray(int size=8, int minValue=0, int maxValue=100)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int[] myArray = CreateRandomArray();
+
+ShowArray(myArray);
