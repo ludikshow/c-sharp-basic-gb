a4327345147706d6820efde619f1ce5c2a1903c4
Lyudmila Karpenko <lk@example.com>
2023-03-09 22:34:05 +0300

    add task 47

---
+++ homeworks/hw7/task47/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int[,] create2DRandomArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] new2DArray = new int[rows,columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for( int j = 0; j < columns; j++)
+        {
+            new2DArray[i,j] = new Random().Next(minValue, maxValue+1);
+        }
+    }
+    return new2DArray;
+}
+
+void print2DRandomArray(int[,] array)
+{
+    for( int i = 0; i < array.GetLength(0); i++)
+    {
+        for( int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine("");
+
+    }
+}
+
+Console.WriteLine("Введите m:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите n:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] result = new int[m, n];
+result = create2DRandomArray(m, n, 0, 10);
+print2DRandomArray(result);
