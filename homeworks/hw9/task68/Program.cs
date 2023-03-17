8c38f4dfb6ed95419d346b3d3217607e6f6ccba4
Lyudmila Karpenko <lk@example.com>
2023-03-17 19:50:24 +0300

    Complete hw9

---
+++ homeworks/hw9/task68/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.WriteLine("Введите два полфункции Аккерманаожительных числа: M и N.");
+int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
+Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+
+static int Akkerman(int n, int m)
+{
+  if ((n == 1) && (m == 1))
+    return 1;
+  else
+    if (n > 1)
+      return Akkerman(n - 1, m) + m;
+    else
+      return Akkerman(n, m - 1) + 1;
+}
